b80debaf886c1e2f738ddd1a0542ec5d4fa0a305
Andranik Markaryan <andranik.mrk@gmail.com>
2022-10-30 22:56:28 +0000

    Able to render search results

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,17 +8,42 @@ input.addEventListener('keyup', function () {
       query: this.value.trim()
     }))
       .then(res => res.json())
-      .then(data => {
-
-      });
+      .then((data: Fighter[]) => drawDropdown(data));
+  } else {
+    clearDropdown();
   }
 });
 
-function drawDropdown(fighters) {
-  dropdown.innerHTML = '';
+function drawDropdown(fighters: Fighter[]) {
+  clearDropdown();
   for (const fighter of fighters) {
     const fightersListItem = document.createElement('li');
-    fightersListItem.
-      dropdown.appendChild()
+    const fighterLink = document.createElement('a');
+    fighterLink.setAttribute('href', fighter.url);
+    fighterLink.setAttribute('target', '_blank');
+    fighterLink.innerText = `${fighter.firstname} ${fighter.lastname} ${fighter.nickname ? `- ${fighter.nickname}` : ''}`;
+    fightersListItem.addEventListener('click', function () {
+      fighterLink.click();
+    })
+    fightersListItem.append(fighterLink);
+    dropdown.appendChild(fightersListItem);
   }
+}
+
+function clearDropdown() {
+  dropdown.innerHTML = '';
+}
+
+type Fighter = {
+  firstname: string
+  lastname: string
+  nickname: string
+  url: string
+  height: string
+  weight: string
+  reach: string
+  win: string
+  loss: string
+  draw: string
+  belt: string
 }
