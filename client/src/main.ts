7ad2aa05f26fa30e7230144c8c40589784bdfdc8
Andranik Markaryan <andranik.mrk@gmail.com>
2022-11-07 22:10:04 +0000

    Add right/left side for comparison of fighters

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,13 @@
-import './styles.css';
-
+import './styles.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+const serverEndpoint = 'http://localhost:3000';
+const RIGHT_SIDE = "right";
+const LEFT_SIDE = "left";
 const input = document.getElementById('input');
 const dropdown = document.getElementById('dropdown');
 input.addEventListener('keyup', function () {
   if (this.value.length > 1) {
-    fetch('http://localhost:3000/search_fighter?' + new URLSearchParams({
+    fetch(serverEndpoint + '/search_fighter?' + new URLSearchParams({
       query: this.value.trim()
     }))
       .then(res => res.json())
@@ -22,13 +25,35 @@ function drawDropdown(fighters: Fighter[]) {
     fighterLink.setAttribute('href', fighter.url);
     fighterLink.setAttribute('target', '_blank');
     fighterLink.innerText = `${fighter.firstname} ${fighter.lastname} ${fighter.nickname ? `- ${fighter.nickname}` : ''}`;
-    fightersListItem.addEventListener('click', function () {
-      fighterLink.click();
-    })
+    const leftSideButton = document.createElement('i');
+    leftSideButton.classList.add('fa-solid', 'fa-arrow-left');
+    leftSideButton.addEventListener('click', function () {
+      addChosenFighterCard(LEFT_SIDE, fighter.firstname, fighter.lastname, fighter.id);
+    });
+    const rightSideButton = document.createElement('i');
+    rightSideButton.classList.add('fa-solid', 'fa-arrow-right');
+    rightSideButton.addEventListener('click', function () {
+      addChosenFighterCard(RIGHT_SIDE, fighter.firstname, fighter.lastname, fighter.id);
+    });
+    fightersListItem.appendChild(leftSideButton);
     fightersListItem.append(fighterLink);
+    fightersListItem.appendChild(rightSideButton);
     dropdown.appendChild(fightersListItem);
   }
 }
+
+function addChosenFighterCard(side, firstname, lastname, fighterId) {
+  const chosenFightersContainer = document.getElementById(`chosen_fighter_${side}`);
+  chosenFightersContainer.innerHTML = '';
+  const chosenFighter = document.createElement('div');
+  chosenFighter.innerHTML = `${firstname} ${lastname}`;
+  chosenFighter.setAttribute('data-fighterid', fighterId);
+  chosenFightersContainer.appendChild(chosenFighter);
+};
+
+function removeChosenFighterCard() {
+
+};
 
 function clearDropdown() {
   dropdown.innerHTML = '';
@@ -46,4 +71,5 @@ type Fighter = {
   loss: string
   draw: string
   belt: string
+  id: string
 }
